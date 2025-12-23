148a0de3bc8c81c27054ce3fdc10d7d420e62a3e
The best is yet to come.                                        try class based backends projects <shashank3fr@gmail.com>
2025-12-23 10:08:31 +0530

    Create Dp | 174. Dungeon Game.cpp

---
+++ LeetCode/Dp | 174. Dungeon Game.cpp
@@ -0,0 +1,43 @@
+neigh always +;
+
+curr cell +, -;
+
+curr cell is + but not enough..
+curr cell is more than enough..
+  
+curr cell is - so i "best/need"  more..
+
+
+sub problem corner will be bottom right..
+
+
+
+class Solution {
+public:
+    int calculateMinimumHP(vector<vector<int>>& dungeon) {
+        int n = dungeon.size();
+        int m = dungeon[0].size();
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, INT_MAX));
+        dp[n][m - 1] = 1;
+        dp[n - 1][m] = 1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 0; j--) {
+                int best = min(dp[i + 1][j], dp[i][j + 1]);
+                int curr = dungeon[i][j];
+                if (curr >= 0) {
+                    if (curr < best) {
+                        dp[i][j] = best - curr; 
+                    } else {
+                        dp[i][j] = 1;
+                    }
+                } else {
+                    // -5    1 abs + best
+                    dp[i][j] = best + abs(curr);
+                }
+            }
+        }
+        return dp[0][0];
+    }
+};
+
