fa8f72d52f25c4149e96911bdd74cbe140093c4b
Shashank601 <shashank3fr@gmail.com>
2026-01-24 14:47:48 +0530

    Create Description Editorial Editorial Solutions Solutions Submissions Submissions Testcase Testcase Test Result Test Result Code Accepted Accepted 189. Rotate Array.cpp

---
+++ LeetCode/Top Interview 150/Description Editorial Editorial Solutions Solutions Submissions Submissions Testcase Testcase Test Result Test Result Code Accepted Accepted 189. Rotate Array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k = k%n;                                         <---- normalized kyuki N*i wali roations se kuch chnage nhi hota
+        reverse(nums.begin(), nums.begin() + (n - k));
+        reverse(nums.begin() + (n - k), nums.end());
+        reverse(nums.begin(), nums.end());
+    }
+};
+
+
+3 methods
+copying, 
+reversing, <-- what i did
+cycling.
